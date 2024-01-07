9b4ac20b25875431a36b04620f002d3286c24f8e
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-01-06 19:39:31 -0500

    implementando componente login (funcionalidad autenticacion de usuario)

--- src/app/auth-components/login/login.component.ts
+++ src/app/auth-components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/auth-services/auth-service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+
+  constructor(private authService: AuthService, private formBuilder: FormBuilder){ }
+
+  ngOnInit(){
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login(){
+    console.log(this.loginForm.value);
+
+    this.authService.login(this.loginForm.value).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    );
+  }
 }
