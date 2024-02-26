99119a539bda815bef4e16bffcb1b709366816fa
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2024-02-26 12:19:19 -0500

    implementando componente view-question (funcionalidad ver pregunta y publicar respuesta)

--- src/app/user/components/view-question/view-question.component.ts
+++ src/app/user/components/view-question/view-question.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { QuestionService } from '../../user-services/question-service/question.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnswerService } from '../../user-services/answer-service/answer.service';
+import { StorageService } from 'src/app/auth-services/storage-service/storage.service';
 
 @Component({
   selector: 'app-view-question',
@@ -11,10 +14,13 @@ export class ViewQuestionComponent {
 
   questionId: number = this.activatedRoute.snapshot.params["questionId"];
   question: any;
+  validateForm: FormGroup;
 
-  constructor(private questionService: QuestionService, private activatedRoute: ActivatedRoute){}
+  constructor(private questionService: QuestionService, private activatedRoute: ActivatedRoute,
+              private formBuilder: FormBuilder, private answerService: AnswerService){}
 
   ngOnInit(){
+    this.validateForm = this.formBuilder.group({body: [null, Validators.required]})
     this.getQuestionById();
   }
 
@@ -23,6 +29,21 @@ export class ViewQuestionComponent {
       (res) => {
         console.log(res);
         this.question = res.questionDto;
+      }
+    );
+  }
+
+  addAnswer(){
+    console.log(this.validateForm.value);
+
+    const data = this.validateForm.value;
+    data.questionId = this.questionId;
+    data.userId = StorageService.getUserId();
+    console.log(data);
+
+    this.answerService.postAnswer(data).subscribe(
+      (res) => {
+        console.log(res);
       }
     );
   }
